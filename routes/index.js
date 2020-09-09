8730923c3b9fbb68a7a31cefb406671d2693c803
jossdz <carloscorrea.dev@gmail.com>
2020-09-09 10:40:05 -0500

    mux demo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router = express.Router()
+const axios = require("axios")
+const Concert = require("../models/Concert")
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index")
+router.get("/", async (req, res, next) => {
+  const concerts = await Concert.find()
+  res.render("index", { concerts })
 })
 
 //1. Ruta para mostrar el formulario para crear el concierto
@@ -11,8 +14,42 @@ router.get("/concert/new", (req, res) => {
   res.render("new-concert")
 })
 //2. Ruta para crear el concierto
-router.post("/concert/new", (req, res) => {})
+router.post("/concert/new", async (req, res) => {
+  const { band, name, duration } = req.body
+  // 2.Generar un nuevo liveStream en Mux
+  const {
+    data: {
+      data: { stream_key, playback_ids, id }
+    }
+  } = await axios.post(
+    "https://api.mux.com/video/v1/live-streams",
+    {
+      playback_policy: "public",
+      new_asset_settings: {
+        playback_policy: "public"
+      }
+    },
+    {
+      auth: {
+        username: process.env.MUX_TOKEN_ID,
+        password: process.env.MUX_TOKEN_SECRET
+      }
+    }
+  )
+  await Concert.create({
+    name,
+    duration,
+    band,
+    streamKey: stream_key,
+    playbackId: playback_ids[0].id,
+    streamId: id
+  })
+  res.redirect("/")
+})
 //3. Ver el concierto
-router.get("/concert/:concertId", (req, res) => {})
+router.get("/concert/:concertId", async (req, res) => {
+  const concert = await Concert.findById(req.params.concertId)
+  res.render("concertDetail", concert)
+})
 
 module.exports = router
